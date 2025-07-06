5f144c81484bbfcd0c5879b0e3d864e28834a3d1
AlexandreResende <alexandre.amres@gmail.com>
2025-07-06 01:08:57 -0300

    chore: Add multiple user generation to seed script

--- src/scripts/mongodb-scripts/seed.js
+++ src/scripts/mongodb-scripts/seed.js
@@ -7,20 +7,25 @@ const { generateOrdersForUser } = require('./utils/generateOrdersForUser');
 (async () => {
   const client = await getConnection();
   const collection = client.db().collection('users');
-  let userId = uuidv4();
-  const createdAt = faker.date.past();
-  const amountOfOrders = faker.number.int({ min: 1, max: 5 });
 
-  const data = {
-    _id: userId,
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    createdAt,
-    updatedAt: faker.date.between({ from: createdAt, to: new Date() }),
-    orders: generateOrdersForUser(amountOfOrders, createdAt),
-  };
+  const amountOfUsers = 2;
 
-  await collection.insertOne(data);
+  for (let count = 0; count < amountOfUsers; count++) {
+    let userId = uuidv4();
+    const createdAt = faker.date.past();
+    const amountOfOrders = faker.number.int({ min: 1, max: 5 });
+
+    const data = {
+      _id: userId,
+      username: faker.internet.username(),
+      email: faker.internet.email(),
+      createdAt,
+      updatedAt: faker.date.between({ from: createdAt, to: new Date() }),
+      orders: generateOrdersForUser(amountOfOrders, createdAt),
+    };
+
+    await collection.insertOne(data);
+  }
 
   return;
 })();
